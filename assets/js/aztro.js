b1e67bcd62549e0af8a32c5f10e364bad5c7954a
rhoverholt <rhoverholt@gmail.com>
2021-12-07 21:27:03 -0500

    implemented lunar phase and zodiakfromdate calculations

--- assets/js/aztro.js
+++ assets/js/aztro.js
@@ -9,6 +9,7 @@ function getAztro(requestUrl) {
 		return (response.json());
 	})
 	.then (function (data) {
+		console.log("Here is the return from aztro");
 		console.log(data.date_range);
 		console.log(data.current_date);
 		console.log(data.description);
@@ -21,5 +22,30 @@ function getAztro(requestUrl) {
 	});
 }
 
-console.log("Here is the return from aztro");
+function getZodiakFromDate(date) {
+
+	console.log("zodiak");
+	console.log(date);
+
+	let month = date.getMonth(); //0-11
+	let day = date.getDate(); //1-31
+
+	console.log(month + "/" + day);
+
+	const zodiak = ["capricorn", "aquarius", "pisces", "aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius"];
+	const zodiakBreakPoints = [20,18,20,19,20,20,22,22,22,22,21,21];
+
+	if (day < zodiakBreakPoints[month]) {
+		return zodiak[month];
+	} 
+	
+	if (month < 11) {
+		return zodiak[month + 1];
+	}
+
+	return zodiak[0];
+}
+
+console.log(getZodiakFromDate(new Date()));
+
 getAztro(aztroUrl);
