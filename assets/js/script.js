3bbc17ebcac328d8cccaa33e0264a964b2312971
Bacilio <b.alvarado@live.com>
2021-12-11 11:00:36 -0500

    cleaned up the time and date function

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,18 +66,13 @@ function showInput() {
     theForm.reset();
 }
 
-function display_ct5() {
-    var x = new Date()
-    var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
-    
-    var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
-    x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds() + ampm;
-    document.getElementById('ct5').innerHTML = x1;
-    display_c5();
+function displayTime() {
+    var time = moment().format('dddd, MMMM Do YYYY, h:mm:ss a')
+
+    $('#clock').html(time);
+    setTimeout(displayTime, 1000);
 }
 
-function display_c5(){
-    var refresh=1000; // Refresh rate in milli seconds
-    mytime=setTimeout('display_ct5()',refresh)
-}
-display_c5(); // displays the date-time, updating every second.
+$(document).ready(function() {
+    displayTime();
+});
