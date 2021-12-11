8403ddf8b302f4ce3b80ab99091b0b645119c25b
kolatham <92490383+kolatham@users.noreply.github.com>
2021-12-11 12:39:18 -0500

    added moon-phase.html and a new js file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fun-with-horoscopes
+
---
+++ assets/js/moon-phase-advice.js
@@ -0,0 +1,7 @@
+var moonPhaseBtnEl = document.getElementById("new-moon-btn")
+var moonPhaseAdviceEl = document.getElementById("advice")
+moonPhaseBtnEl.addEventListener("click", function() {
+    // If mode is dark, apply light background {
+      moonPhaseAdviceEl.removeAttribute("class", "hide");
+    }
+) 
