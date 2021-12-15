0075e56745feca92e138833160bc0cae83ef88fc
kolatham <92490383+kolatham@users.noreply.github.com>
2021-12-14 21:09:16 -0500

    added javascript changed buttons and p class in moon-phases-html

--- assets/js/moon-phase-advice.js
+++ assets/js/moon-phase-advice.js
@@ -1,7 +1,25 @@
-var moonPhaseBtnEl = document.getElementById("new-moon-btn")
-var moonPhaseAdviceEl = document.getElementById("advice")
-moonPhaseBtnEl.addEventListener("click", function() {
-    // If mode is dark, apply light background {
-      moonPhaseAdviceEl.removeAttribute("class", "hide");
-    }
-) 
+var moonPhaseBtnEl = document.getElementById("moon-phases");
+var displayAdvice = "";
+moonPhaseBtnEl.addEventListener("click", showMoonPhase);
+  
+function showMoonPhase(event) {
+  if (event.target.id == null){
+    return
+  }   
+  
+  if (event.target.tagName != "BUTTON"){
+    return
+  }
+console.log(displayAdvice)
+  if (displayAdvice != ""){
+    var hideEl = document.getElementById(displayAdvice)
+    hideEl.classList.add("hide")
+  
+  }
+  var adviceEl = document.getElementById(event.target.id.substring(0, event.target.id.length-4)+"-p")
+  adviceEl.removeAttribute("class", "hide" )
+  displayAdvice = adviceEl.id
+  
+
+}
+
